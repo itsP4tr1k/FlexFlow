a3bc4469656204829381769f137cd5a9d0ae4180
itsP4tr1k <p4tr1k.ytb@gmail.com>
2024-02-28 17:21:36 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const text = document.getElementById("title-effect");
+let i = 1;
+
+function changeWord() {
+  const words = ["Mozgás", "Közösség", "Sport", "Család"];
+  const word = `${words[i]}.`;
+
+  text.innerText = word;
+
+  if (i + 1 == words.length) {
+    i = 0;
+  } else {
+    i++;
+  }
+}
+
+window.setInterval(() => {
+  changeWord();
+}, 3000);
+
+function toggleNavbar() {
+  if (document.getElementById("navbar").dataset.show === "true") {
+    document.getElementById("navbar").dataset.show = "false";
+  } else {
+    document.getElementById("navbar").dataset.show = "true";
+  }
+}
+
+function setPage(page) {
+  window.location.href = `#${page}`;
+}
